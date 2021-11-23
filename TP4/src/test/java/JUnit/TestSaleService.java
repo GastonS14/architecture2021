0ebb92f2c7398c2b95ca66f1eac3bd20c171fd83
Juan Segundo Castaño Rossi <juansec@live.com.ar>
2021-11-23 15:33:30 -0300

    fixing sale test

--- TP4/src/test/java/JUnit/TestSaleService.java
+++ TP4/src/test/java/JUnit/TestSaleService.java
@@ -1,61 +1,83 @@
 package JUnit;
 
-import com.integrador4.dto.ProductRequest;
 import com.integrador4.dto.RequestSale;
 import com.integrador4.dto.SaleProductDto;
+import com.integrador4.entity.Client;
 import com.integrador4.entity.Product;
 import com.integrador4.entity.Sale;
-import com.integrador4.service.ProductService;
+import com.integrador4.repository.*;
 import com.integrador4.service.SaleService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest( classes = {SaleService.class,ProductService.class})
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith( MockitoExtension.class )
+@ExtendWith(SpringExtension.class )
 public class TestSaleService {
-
-    private static RequestSale rs;
-    private static ArrayList<SaleProductDto> saleProduct;
-    @Autowired
-    private static SaleService saleService;
-    @Autowired
-    private static ProductService productService;
-    private static double amountSale;
+    static int productUnits;
+    static int idClient;
+    static int idProduct;
+    static int initialStock;
+    static ArrayList<SaleProductDto> saleProduct;
+    static Sale sale;
+    @MockBean SaleRepository saleRepository;
+    @MockBean ProductRepository productRepository;
+    @MockBean ClientRepository clientRepository;
+    @MockBean RequestRepository requestRepository;
+    @InjectMocks SaleService saleService;
 
     @BeforeAll
-    static void loadData() {
-        Optional<Product> p = productService.save( new ProductRequest("mayonesa",200.3,25) );
-        Optional<Product> p1 = productService.save( new ProductRequest("queso",641.3,50) );
-        saleProduct.add( new SaleProductDto( 2, p.get().getIdProduct()));
-        saleProduct.add( new SaleProductDto( 3, p1.get().getIdProduct()));
-        amountSale = p.get().getPrice() * 2 + p1.get().getPrice() * 3;
+    static void loadData () {
+        saleProduct = new ArrayList<>();
+        sale = new Sale();
+        productUnits = 2;
+        idClient = 9;
+        idProduct = 2;
+        initialStock = 25;
     }
+
 
     @Test()
     @DisplayName( "adding new sale")
     public void addingSale(){
-        rs = new RequestSale( 9, saleProduct );
-        Optional<Sale> sale = saleService.save( rs );
-        assertTrue( !sale.isEmpty(), "Should save the purchase" );
-        assertTrue( sale.get().getAmount() == amountSale, "Amount should be: "+ amountSale );
+        Client client = new Client("juan","segundo");
+        Product product = new Product("mayonesa",200.3,initialStock);
+        sale.setClient( client );
+        double amountSale = product.getPrice() * productUnits;
+        sale.setAmount(amountSale);
+        Mockito.when( saleRepository.save( sale )).thenReturn( sale );
+        Mockito.when( clientRepository.findById(idClient)).thenReturn( Optional.of( client ));
+        Mockito.when( productRepository.findById(idProduct)).thenReturn( Optional.of( product ) );
+        Mockito.when( productRepository.save(product)).thenReturn(  product );
+        Mockito.when( requestRepository.getPurchases(Date.valueOf(LocalDate.now()), idClient))
+                .thenReturn( new ArrayList<>());
+        saleProduct.add( new SaleProductDto( productUnits, idProduct ));
+        RequestSale rs = new RequestSale(9, saleProduct);
+        Optional<Sale> expected = saleService.save(rs);
+        assertFalse( expected.isEmpty(), "Should save the purchase");
+        assertEquals( expected.get().getAmount(), amountSale, "Amount should be: " + amountSale);
+        assertEquals( expected.get().getClient().getName(), client.getName(), "Name should be juan");
+        assertTrue( product.getStock() < initialStock );
     }
 
     @Test()
     @DisplayName( "adding wrong product")
     public void addingWrongProduct() {
-        saleProduct.clear();
-        Optional<Product> p = productService.save( new ProductRequest("tita", 70.7,50) );
-        saleProduct.add( new SaleProductDto( 50, p.get().getIdProduct()) );
-        Optional<Sale> sale = saleService.save( new RequestSale(9, saleProduct ) );
-        assertTrue( sale.isEmpty(), "Shouldn't save this sale cause has more than 3 units" );
+        // to do
     }
+
+
 
 }
